259be6dca4575ad623a32bc938bc36781fbd813b
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-09-14 19:09:56 -0400

    Create Two Pointer(2).js

---
+++ JavaScript/Algorithm/Two Pointer(2).js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let pos = 0;
+    
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i]!=0){nums[pos++] = nums[i];}
+    }
+    
+    for(let i = pos; i<nums.length; i++){
+        nums[i] = 0;
+    }
+};
